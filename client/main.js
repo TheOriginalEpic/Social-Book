ca1bde4d26c11042b5ad2fe43421755194de6d32
TheOriginalEpic <thepicflashback@gmail.com>
2019-01-31 15:57:41 -0400

    added like and dislike button with an image

--- client/main.js
+++ client/main.js
@@ -3,20 +3,29 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+Template.description.events({
+	'click .js-like'(event, instance) {
+    console.log("You clicked like");
+	},
+	'click .js-dislike'(event, instance) {
+	console.log("You clicked dislike");
+	},
 });
