360aa8df76399d9794d7163b5c75ab59ec9ffe56
TheOriginalEpic <thepicflashback@gmail.com>
2019-02-07 15:42:11 -0400

    Add a way to insert a proflie by use of a modal

--- client/main.js
+++ client/main.js
@@ -2,30 +2,25 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
+import '../lib/collections.js';
 
 Template.description.events({
 	'click .js-like'(event, instance) {
-    console.log("You clicked like");
+    	console.log("You clicked like");
 	},
 	'click .js-dislike'(event, instance) {
-	console.log("You clicked dislike");
+		console.log("You clicked dislike");
+	},	
+});
+
+Template.addUser.events({
+	'click .js-save'(event, instance) {
+		var fName = $('#addUser input[name="firstName"]').val();
+		var lName = $('#addUser input[name="lastName"]').val();
+		var image = $('#addUser input[name="imageURL"]').val();
+		console.log("Name: ", fName, lName);
+		console.log("Picture: ", image);
+
+		$('#addUser').modal('hide');
 	},
 });
