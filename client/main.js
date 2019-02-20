7a25789ad61d9414678a62cb566d92ee2008b328
TheOriginalEpic <thepicflashback@gmail.com>
2019-02-20 15:41:14 -0400

    added the ability to add more users

--- client/main.js
+++ client/main.js
@@ -5,18 +5,25 @@ import './main.html';
 import '../lib/collections.js';
 
 Template.description.helpers({
-	descfName(){
-		return userDB.findOne({}).firstName;
+	descAll(){
+		return userDB.find({});
 	},
 });
 
 Template.description.events({
-	'click .js-like'(event, instance) {
+	'click .js-like'(event, instance){
     	console.log("You clicked like");
 	},
-	'click .js-dislike'(event, instance) {
+	'click .js-dislike'(event, instance){
 		console.log("You clicked dislike");
 	},	
+	'click .js-delete'(event, instane){
+		var descID = this._id;
+
+		$("#" + descID).fadeOut("slow", "swing", function () {
+			userDB.remove({_id: descID});
+		});
+	},
 });
 
 Template.addUser.events({
@@ -24,6 +31,10 @@ Template.addUser.events({
 		var fName = $('#addUser input[name="firstName"]').val();
 		var lName = $('#addUser input[name="lastName"]').val();
 		var image = $('#addUser input[name="imageURL"]').val();
+		console.log(image);
+		if (image==""){
+			image="Undertale 2.jpg";
+		}
 		console.log("Name: ", fName, lName);
 		console.log("Picture: ", image);
 
@@ -34,7 +45,5 @@ Template.addUser.events({
 		$('#addUser').modal('hide');
 
 		userDB.insert({'firstName':fName, 'lastName':lName, 'img':image});
-
-
 	},
 });
