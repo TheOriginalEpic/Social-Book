4e559a791fca47a7b99cd87d36d7fa72235eb6ba
TheOriginalEpic <thepicflashback@gmail.com>
2019-02-28 13:36:48 -0400

    broke it

--- client/main.js
+++ client/main.js
@@ -5,16 +5,23 @@ import './main.html';
 import '../lib/collections.js';
 
 Template.description.helpers({
+
 	descAll(){
 		return userDB.find({});
 	},
+	isLikeSingle:function(num){
+		return (num <= 1);
+	},
+	isDislikeSingle:function(num){
+		return (num <= 1);
+	}
 });
 
-// Template.editUser.helpers({
-// 	editAll(){
-// 		return userDB.find({});
-// 	},
-// });
+Template.editUser.helpers({
+	editAll(){
+		return userDB.findOne({});
+	},
+});
 
 Template.description.events({
 	'click .js-like'(event, instance){				
@@ -33,6 +40,7 @@ Template.description.events({
 
 		if (add == 1){
 			oneLike ='Like';
+
 		}		
 
 		userDB.update({_id: descID}, {$set:{'like':count, 'text2': oneLike}});
@@ -57,10 +65,15 @@ Template.description.events({
 		}		
 
 		userDB.update({_id: descID}, {$set:{'dislike':count, 'text': oneDislike}});
-	},
+	},	
 
-	'click #edit'(event, instance) {
-		
+	'click .edit'(event, instance){
+		var userId = this._id;
+		console.log(userId);
+
+		$('#uId').val(userId);
+		$('#backImg').attr('src',userDB.findOne({_id:userId}).img);
+		$('#userImg').attr('src',userDB.findOne({_id:userId}).prof);				
 	},
 });
 
@@ -68,7 +81,12 @@ Template.addUser.events({
 	'click .js-save'(event, instance){
 		var fName = $('#addUser input[name="firstName"]').val();
 		var lName = $('#addUser input[name="lastName"]').val();
-		var image = $('#addUser input[name="imageURL"]').val();			
+		var image = $('#addUser input[name="imageURL"]').val();
+		var profileImg = $('#addUser input[name="profileImg"]').val();
+
+		if (profileImg = ""){
+			profileImg = "blank-profile.png";
+		}
 
 		if (image == ""){
 			image = "Undertale 2.jpg";
@@ -77,12 +95,11 @@ Template.addUser.events({
 		$('#addUser input[name="firstName"]').val('');
 		$('#addUser input[name="lastName"]').val('');
 		$('#addUser input[name="imageURL"]').val('');
+		//$('#addUser input[name="profileImg"]').val('');
 
 		$('#addUser').modal('hide');
 
-		userDB.insert({'firstName':fName, 'lastName':lName, 'img':image,});
-
-
+		userDB.insert({'firstName':fName, 'lastName':lName, 'img':image, 'prof': profileImg});
 	},
 });
 
@@ -98,7 +115,14 @@ Template.editUser.events({
 	},
 
 	'click .js-edit'(event, instance){
+		// var efName = $('#editing input[name="eFirstName"]').val();
+		// var elName = $('#editing input[name="eLastName"]').val();
+		// var eImage = $('#editing input[name="eImageURL"]').val();
+		// var profileImg = $('#editing input[name="eProfileImg"]').val();
+
 		$('#editing').modal('handleUpdate');
+
+		//userDB.insert({'firstName':efName, 'lastName':elName, 'img':eimage, 'profileImg': profileImg});
 
 	},
 
